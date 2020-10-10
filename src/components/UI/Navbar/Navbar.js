2be7776ae0492b947bc831ea49c9dece7374cd84
Ali <khanxesthetix@gmail.com>
2020-10-11 00:50:08 +0530

    Added header styling

--- src/components/UI/Navbar/Navbar.js
+++ src/components/UI/Navbar/Navbar.js
@@ -4,24 +4,27 @@ import { useDispatch } from "react-redux";
 import { selectAction } from "../../../store/actions/actions";
 import Logo from "../../../images/BB_S5_excl_namelab_c.jpg";
 
-import Select from 'react-select';
+import Select from "react-select";
 
 const CharacterOptions = [
-    { value: '-----------', label: '-----------' },
-    { value: 'Get character by id', label: 'Get character by id' },
-    { value: 'Get character by category', label: 'Get character by category' },
-    { value: 'Get random character', label: 'Get random character' },
-    { value: 'Search character by name', label: 'Search character by name' },
-  ];
+  { value: "-----------", label: "-----------" },
+  { value: "Get character by id", label: "Get character by id" },
+  { value: "Get character by category", label: "Get character by category" },
+  { value: "Get random character", label: "Get random character" },
+  { value: "Search character by name", label: "Search character by name" },
+];
 
-  const QuoteOptions = [
-    { value: '-----------', label: '-----------' },
-    { value: 'Get quote by id', label: 'Get quote by id' },
-    { value: 'Get quotes by series', label: 'Get quotes by series' },
-    { value: 'Get a random quote', label: 'Get a random quote' },
-    { value: 'Get quote by author', label: 'Get quote by author' },
-    { value: 'Get a random quote by author', label: 'Get a random quote by author' },
-  ];
+const QuoteOptions = [
+  { value: "-----------", label: "-----------" },
+  { value: "Get quote by id", label: "Get quote by id" },
+  { value: "Get quotes by series", label: "Get quotes by series" },
+  { value: "Get a random quote", label: "Get a random quote" },
+  { value: "Get quote by author", label: "Get quote by author" },
+  {
+    value: "Get a random quote by author",
+    label: "Get a random quote by author",
+  },
+];
 
 const Navbar = (props) => {
   const dispatch = useDispatch();
@@ -63,19 +66,23 @@ const Navbar = (props) => {
         <ul>
           <li>
             <label>Characters</label>
-            <Select
-              disabled={selectedQuote}
-              onChange={dropDownCharacterSelected}
-              options={CharacterOptions}
-            />
+            <div>
+              <Select
+                disabled={selectedQuote}
+                onChange={dropDownCharacterSelected}
+                options={CharacterOptions}
+              />
+            </div>
           </li>
           <li>
             <label>Quotes</label>
-            <Select
-              disabled={selectedCharacter}
-              onChange={dropDownQuoteSelected}
-              options={QuoteOptions}
-            />
+            <div className={classes.Select}>
+              <Select
+                disabled={selectedCharacter}
+                onChange={dropDownQuoteSelected}
+                options={QuoteOptions}
+              />
+            </div>
           </li>
         </ul>
       </div>
